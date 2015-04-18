6ec7be9fa60e39ec6e8f72e14a81de156493fc27
tiagoprn <tiagoprn@gmail.com>
2015-04-18 16:35:35 -0300

    Fine tuning of the templates.

--- sitebuilder.py
+++ sitebuilder.py
@@ -30,6 +30,17 @@ def tag(tag):
     return render_template('tag.html', pages=tagged, tag=tag)
 
 
+@app.route('/tags')
+def tags():
+    tags_list = []
+    for p in pages:
+        page_tags = p.meta.get('tags', [])
+        for t in page_tags:
+            tags_list.append(t)
+    unique_tags = sorted(set(tags_list))
+    return render_template('tags.html', all_tags=unique_tags)
+
+
 if __name__ == "__main__":
     if len(sys.argv) > 1 and sys.argv[1] == "build":
         freezer.freeze()
