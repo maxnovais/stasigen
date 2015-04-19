9bed25e2708eb08002b58006d7f3b319df321a6b
tiagoprn <tiagoprn@gmail.com>
2015-04-19 00:03:12 -0300

    explicitly saying what is a page.

--- sitebuilder.py
+++ sitebuilder.py
@@ -30,19 +30,19 @@ def index():
     return render_template('index.html', flatpages=flatpages)
 
 
-@app.route('/<path:path>/')
+@app.route('/pages/<path:path>/')
 def page(path):
     page = flatpages.get_or_404(path)
     return render_template('page.html', page=page)
 
 
-@app.route('/tag/<string:tag>/')
+@app.route('/pages/tag/<string:tag>/')
 def tag(tag):
     tagged = [p for p in flatpages if tag in p.meta.get('tags', [])]
     return render_template('tag.html', flatpages=tagged, tag=tag)
 
 
-@app.route('/tags/')
+@app.route('/pages/tags/')
 def tags():
     tags_list = []
     for p in flatpages:
